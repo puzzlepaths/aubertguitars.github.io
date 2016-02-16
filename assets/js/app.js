f9bfd489ed16889e14645611166cfa8906c3d62d
gregaubs <gaj.aubert@gmail.com>
2016-02-16 12:17:38 +0000

    mobile bug

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,11 @@
 $( document ).ready(function() {
 
-
+	var bg = jQuery("#background1");
+	jQuery(window).resize("resizeBackground");
+	function resizeBackground() {
+	    bg.height(jQuery(window).height() + 60);
+	}
+	resizeBackground();
 
 
 //end Jquery
