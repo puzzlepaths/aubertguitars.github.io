758d0d38ada7a17f1749f2570ce8df3ddfc123a6
gregaubs <gaj.aubert@gmail.com>
2016-02-16 12:22:52 +0000

    m b

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,7 +3,7 @@ $( document ).ready(function() {
 	var bg = jQuery("#background1");
 	jQuery(window).resize("resizeBackground");
 	function resizeBackground() {
-	    bg.height(jQuery(window).height() + 60);
+	    bg.height(jQuery(window).height() + 120);
 	    console.log('resize');
 	}
 	resizeBackground();
